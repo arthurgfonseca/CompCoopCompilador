79fc356170b80d53a967009028399695fa5990b4
Arthur <arthur.fonseca@i.ndigo.com.br>
2011-09-21 15:38:34 -0300

    Initial Commit

---
+++ Compilador/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Compilador
+//
+//  Created by Arthur Fonseca on 9/21/11.
+//  Copyright 2011 Poli USP. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
